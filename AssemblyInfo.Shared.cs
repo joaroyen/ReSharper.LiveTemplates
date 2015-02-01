fdd4f41f96d0f3d6e8d3821fbe00d2bf3d141609
joaroyen <joar.oyen@joaroyen.com>
2015-02-01 11:05:26 +0100

    Added shared AssemblyInfo to assemblies

---
+++ AssemblyInfo.Shared.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyDescription("Macros for ReSharper Platform Live Templates")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Joar Øyen")]
+[assembly: AssemblyProduct("JoarOyen.ReSharper.LiveTemplates")]
+[assembly: AssemblyCopyright("Copyright © 2015 Joar Øyen")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
