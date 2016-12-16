d72a51f3de9c072622b0ff87c2ecdf79c7033e0e
Joar Øyen <joar.oyen@joaroyen.com>
2016-12-16 08:30:27 +0100

    Updated version number to 7

--- AssemblyInfo.Shared.cs
+++ AssemblyInfo.Shared.cs
@@ -4,9 +4,9 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Joar Øyen")]
 [assembly: AssemblyProduct("JoarOyen.ReSharper.LiveTemplates")]
-[assembly: AssemblyCopyright("Copyright © 2015 Joar Øyen")]
+[assembly: AssemblyCopyright("Copyright © 2017 Joar Øyen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
